f4918368bb033eb411580e01b015e2b880b21d0d
romdit2 <48025629+romdit2@users.noreply.github.com>
2020-09-05 18:35:10 +0300

    Update MachineLearning.py

--- MachineLearning.py
+++ MachineLearning.py
@@ -36,10 +36,10 @@ class K_Means(ML):
 
     def fit(self,data1):
         data = np.array(data1)
-        self.centroids = {}
+        self.centroids = self.initialize(data, self.k)
 #initialization of centroids
-        for i in range(self.k):
-                self.centroids[i] = data[i]
+        #for i in range(self.k):
+                #self.centroids[i] = data[i]
 #optimization process
         for i in range(self.max_iter):
 #we are clearing the classifications dictionary for every iteration because for every iteration the centroids move
@@ -69,9 +69,9 @@ class K_Means(ML):
                             optimized = False
 #if optimized equals true it means that the centroid moved very little and we can stop optimizing their placement
             if optimized:
-                g =self.inertia()
+                #g =self.inertia()
                 #print(self.inertia_values)
-                self.inertia_values = g
+                #self.inertia_values = g
                 #print(self.inertia_values)
                 print(i)
                 break
@@ -89,6 +89,31 @@ class K_Means(ML):
                 distance = np.linalg.norm(cluster_point - centroid)
                 cluster_sum_of_squares_points_to_clusters += distance**2
         return cluster_sum_of_squares_points_to_clusters
+    
+    def initialize(self, X, K):
+        """
+        Create cluster centroids using the k-means++ algorithm.
+            X : numpy array
+                The dataset.
+        """
+		C = {}
+		C[0] = X[0]
+		z = 1
+		for k in range(1, K):
+			#calculates the distance squared from an element in the dataset to the existing centroids
+			D2 = scipy.array([min([scipy.inner(c-x,c-x) for c in C]) for x in X])
+			#calculates the probability to get an element in the right distance from the centroids
+			probs = D2/D2.sum()
+			cumprobs = probs.cumsum()
+			#finds an element randomly in the right distance
+			r = scipy.rand()
+			for j,p in enumerate(cumprobs):
+				if r < p:
+					i = j
+					break
+			C[z] = X[i]
+			z += 1
+		return C
 
 # A simple, temporary implementation of the ML class by using the KMeans algorithm provided by sklearn toolkit
 class TempML(ML):
